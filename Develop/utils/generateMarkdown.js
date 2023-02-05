ddfddc988f056affe111ddce8f26b2b14863939f
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-02-05 20:08:10 +1100

    GenerateMarkdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,48 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'GPL':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'Apache':
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    default:
+      return '';
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'GPL':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    case 'Apache':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    default:
+      return '';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) return '';
 
-// TODO: Create a function to generate markdown for README
+  const badge = renderLicenseBadge(license);
+  const link = renderLicenseLink(license);
+
+  return `## License
+
+This project is licensed under the ${license} license. ${badge}
+
+For more information, see [${license} license](${link}).`;
+}
+
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+${data.description}
+
+${renderLicenseSection(data.license)}`;
 }
 
 module.exports = generateMarkdown;
